361da2e1ec0bde1c24426ddd1eefbfa47c7b3718
Denis Bešić <besicdenis@gmail.com>
2018-08-28 10:15:32 +0200

    Create go_install.sh

---
+++ go_install.sh
@@ -0,0 +1,6 @@
+#Install go
+wget https://dl.google.com/go/go1.11.linux-amd64.tar.gz
+sudo tar -C /usr/local/bin -xzf go1.11.linux-amd64.tar.gz
+rm go1.11.linux-amd64.tar.gz
+echo "export PATH=$PATH:/usr/local/go/bin" > /etc/profile 
+source /etc/profile
